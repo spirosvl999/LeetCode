cef60ee47dd5dd3bdbb233c437e78bb1f2e8cc73
spirosvl999 <79493906+spirosvl999@users.noreply.github.com>
2023-08-01 23:05:39 +0300

    Create 13_Roman_to_Integer.cpp

---
+++ C++/13_Roman_to_Integer.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    int romanToInt(string s) 
+    {
+       int sum =  0;
+       for(int i=0; i<= s.length(); i++)
+       {
+           if(s[i]=='I')
+           {
+               if(s[i+1] == 'V'|| s[i+1] == 'X')
+               {
+                   sum = sum - 1;
+               }
+               else
+               {
+                   sum = sum + 1;
+               }
+           }
+            else if(s[i]=='V')
+           {
+               sum = sum + 5;
+           }
+           else if(s[i]=='X')
+           {
+               if(s[i+1] == 'L'  || s[i+1] == 'C')
+               {
+                   sum = sum - 10;
+               }
+               else
+               {
+                    sum = sum + 10;
+               }
+           }
+           else if(s[i] == 'L')
+           {
+               sum = sum + 50;
+           }
+           else if(s[i] == 'C')
+           {
+               if(s[i+1] == 'D' || s[i+1] == 'M')
+               {
+                   sum = sum - 100;
+               }
+               else
+               {
+                   sum = sum + 100;
+               }
+           }
+           else if(s[i] == 'D')
+           {
+               sum = sum + 500;
+           }
+           else if(s[i] == 'M')
+           {
+               sum = sum + 1000;
+           }
+       }
+       return sum;
+    }
+};
