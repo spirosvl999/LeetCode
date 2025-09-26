7c3937fdb12ec5368905e99100149f17cf8deb2f
Vlachos Spiridon <79493906+spirosvl999@users.noreply.github.com>
2025-09-26 16:50:40 +0300

    Update 167_Two_Sum_II.py

--- Python/167_Two_Sum_II.py
+++ Python/167_Two_Sum_II.py
@@ -1,6 +1,6 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        for i in range(len(numbers)):
+        for i in range(len(numbers)):                                       # Classic Binary Search
             l = i + 1
             r = len(numbers) - 1
             temp = target - numbers[i]
